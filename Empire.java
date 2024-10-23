39f75ea8f0345befa6a8c427a0b00ed7a329ae94
sahana727 <santhoshshetty254@gmail.com>
2024-10-23 21:18:40 +0530

    Update Empire.java

--- Empire.java
+++ Empire.java
@@ -1,71 +1,48 @@
-public class Empire{
-	
-	public static void main(String args[]){
+public class Empire {
 
-	int [] arr = {10 , 5 , 17 , 3 , 20};
-	
+	 public static void main(String[] args) {
+	        int[] arr = {2, 12, 5, 4, 25, 10}; 
+	        int n = arr.length; 
+	        int sum = SumOfArray(arr, n); 
+	        System.out.println("Sum of array: " + sum);
+	        evenOddCount(arr, n);
+	        String name = "Mangalore";
+	        char ch = 'g';
+	        int result = findingFirstOccurance(name,ch);
+	        System.out.println("The charector occured at the position : "+result);
+	    }
 
-	System.out.println("the sum is:"+ summ(arr));
-	
-
-    String value="Mangluru";
-	char ch='g';
-	System.out.println("Occurrence of Character is:" + foundOccurance(value, ch));
-
-	
-
-
-int evencount=0;
-	int oddcount=0;
-
-	for(int i=0;i<arr.length;i++)
-	{
-		if(arr[i]%2==0)
-		{
-			evencount++;
-
-		}
-		else
-		{
-			oddcount++;
-		}
-	}
-
-System.out.println("the even value is:"+ evencount);
-System.out.println("the odd value is:"+ oddcount);
-
-}
-public static int summ(int [] arr )
-{
-	
-	int sum =0;
-	for (int i=0;i<arr.length;i++)
-	{
-		sum = sum+arr[i];
-
-	}
-
-     return sum;
-}
-
-public static int foundOccurance(String value ,char ch)
- {
-		char[] charArray = value.toCharArray();
-		int n = charArray.length;
-		for(int i=0;i<n;i++)
-		{
-			if(charArray[i] == ch)
-			{
-				
-				return i;
-			}
-
-        }
-
-         System.out.println("Charector not found");
-         return -1;
-}
-
-
-
+	    
+	    public static int SumOfArray(int[] arr, int n) {
+	        int sum = 0;
+	        for (int i = 0; i < n; i++) {
+	            sum = sum + arr[i];  
+	        }
+	        return sum;  
+	    }
+	    public static void evenOddCount(int[] arr, int n) {
+	        int evenCount = 0;
+	        int oddCount = 0;
+	        
+	        for (int i = 0; i < n; i++) {
+	            if (arr[i] % 2 == 0) {
+	                evenCount++;  
+	            } else {
+	                oddCount++;   
+	            }
+	        }
+	        
+	        System.out.println("Even number count: " + evenCount);
+	        System.out.println("Odd number count: " + oddCount);
+	    }
+	    public static int findingFirstOccurance(String name, char ch) { 
+	    	char[] chArray = name.toCharArray();
+	    	int n = chArray.length;
+	    	for(int i = 0;i < n;i++) {
+	    		if(chArray[i]==ch) {
+	    			return i;
+	    		}
+	    	}
+	    	return -1;
+	    }
 }
